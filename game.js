6350dce83b080b27d04197fa38542bda79024f25
Steven Reed <iwanttoemailsteven@gmail.com>
2014-07-19 12:47:05 -0700

    Added symbolic stuff

--- game.js
+++ game.js
@@ -18,7 +18,6 @@ function create () {
     GD.player=game.add.sprite(0,0,game.cache.getBitmapData('delorean'));
     GD.fun='';
     GD.running=false;
-
 }
 
 function update() {
@@ -33,9 +32,33 @@ function makeColoredRect(key, width, height, r, g, b) {
 }
 
 function makeRect(key, width, height) {
-	debugger;
 	var r = game.rnd.between(0,255);
 	var b = game.rnd.between(0,255);
 	var g = game.rnd.between(0,255);
 	return makeColoredRect(key, width, height, r,g,b);
+}
+
+//Parsing Functions
+
+// s = textbox.getText();
+// f = symToFn(s);
+// for(int x=0;x<canvas.width;x++) {
+// 	canvas.drawPixel(x,f(x));
+// }
+
+function symToFn(string) {
+	//  split polynomial on [+-]
+	// ex: '5x^2 + 3x - 10' -> var parts = [{c:5,e:2},true,{c:3,e:1},false,{c:10,e:0}]
+	return function(x) {
+		var total = evalFactor(parts[0],x);
+		for(int i=1;i<total.size();i+=2) {
+			if (parts[i]) total+=evalFactor(parts[i+1],x);
+			else total-=evalFactor(parts[i+1],x);
+		}
+		return total;
+	}
+}
+
+function evalFactor(f,x) {
+	return f.c*Math.pow(x,f.e);
 }
