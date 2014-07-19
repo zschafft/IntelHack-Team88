be66541a50f8d62e8c013413b9eb40e481085d5b
Steven Reed <iwanttoemailsteven@gmail.com>
2014-07-19 12:25:20 -0700

    Updated phaser.js. Added makeRect

--- game.js
+++ game.js
@@ -1,31 +1,41 @@
-GD = {};
+var game;
+var GD = {};
 
-function MathGame() {
-
-        var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
-
-        function preload () {
-
-            game.load.image('logo', 'phaser.png');
-
-        }
-
-        function create () {
-
-            // var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-            // logo.anchor.setTo(0.5, 0.5);
-
-            
-            GD.player=game.add.sprite(0,0,'delorean');
-            GD.fun='';
-            GD.running=false;
-
-        }
-
-        function update() {
-        	//update loop
-        }
-
+window.onload = function() {
+    game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 };
 
-window.onload = MathGame;
+// Game Callbacks -----
+function preload () {
+    game.load.image('logo', 'phaser.png');
+}
+
+function create () {
+    // var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+    // logo.anchor.setTo(0.5, 0.5);
+
+    makeRect('delorean',50,50);
+    GD.player=game.add.sprite(0,0,game.cache.getBitmapData('delorean'));
+    GD.fun='';
+    GD.running=false;
+
+}
+
+function update() {
+	//update loop
+}
+//----------
+
+// Utils
+function makeColoredRect(key, width, height, r, g, b) {
+	var rect = game.make.bitmapData(width,height,key,true);
+	rect.fill(r,g,b,1);
+}
+
+function makeRect(key, width, height) {
+	debugger;
+	var r = game.rnd.between(0,255);
+	var b = game.rnd.between(0,255);
+	var g = game.rnd.between(0,255);
+	return makeColoredRect(key, width, height, r,g,b);
+}
