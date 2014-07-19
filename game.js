a08d8487522883c05b8b3fb9a29f84125811abb0
zach schaffter <zschafft@purdue.edu>
2014-07-19 12:27:13 -0700

    add button

--- game.js
+++ game.js
@@ -4,7 +4,7 @@ window.onload = function() {
 
         function preload () {
 
-            game.load.image('logo', 'phaser.png');
+            //game.load.image('logo', 'phaser.png');
 
         }
 
@@ -12,6 +12,18 @@ window.onload = function() {
 
             var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
             logo.anchor.setTo(0.5, 0.5);
+
+            //button = game.add.button(x='400',y='400', callback=Click_Button);
+        
+            button = game.add.button(game.world.centerX, game.world.centerY, 'button', Click_Button, this, 2, 1, 0);
+
+    		//	Set the anchor of the sprite in the center, otherwise it would rotate around the top-left corner
+    		button.anchor.setTo(0.5, 0.5);
+        }
+
+        function Click_Button(){
+
+        	alert("Cicked on button!");
 
         }
 
